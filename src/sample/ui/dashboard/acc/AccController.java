2e476e5268985a30abed33358ad3921dbf78b254
ncanu <ncanu@miumg.edu.gt>
2018-04-17 03:37:39 -0600

    create account

--- src/sample/ui/dashboard/acc/AccController.java
+++ src/sample/ui/dashboard/acc/AccController.java
@@ -2,8 +2,18 @@ package sample.ui.dashboard.acc;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class AccController {
 
@@ -27,4 +37,18 @@ public class AccController {
 
     @FXML
     private JFXButton editAccountButton;
+
+    @FXML
+    void addAccount(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample/ui/dashboard/acc/create/createAcc.fxml"));
+        Node source = (Node)  event.getSource();
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner((Stage) source.getScene().getWindow());
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Cuenta Bancaria");
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 }
