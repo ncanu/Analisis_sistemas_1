e0d2ddc73c4b05226485d7ef3ce072e67745fa9c
ncanu <ncanu@miumg.edu.gt>
2018-04-16 23:51:37 -0600

    new FXML added

--- src/sample/ui/dashboard/acc/AccController.java
+++ src/sample/ui/dashboard/acc/AccController.java
@@ -1,4 +1,30 @@
 package sample.ui.dashboard.acc;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AccController {
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> number;
+
+    @FXML
+    private TableColumn<?, ?> type;
+
+    @FXML
+    private TableColumn<?, ?> bank;
+
+    @FXML
+    private TableColumn<?, ?> balance;
+
+    @FXML
+    private JFXButton addAccount;
+
+    @FXML
+    private JFXButton editAccountButton;
 }
